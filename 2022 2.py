2a0899a1ba497b33ba6d9483eba982ceb0086477
yvonne012 <98299722+yvonne012@users.noreply.github.com>
2022-01-24 12:43:00 +0800

    Add files via upload

---
+++ 2022 2.py
@@ -0,0 +1,2 @@
+print(76+67+11+86)
+print('hello world')
---
+++ 2022 3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 24 11:19:37 2022
+
+@author: Yvonne
+"""
+
+print("I'm Yvonne She is "cool"")
